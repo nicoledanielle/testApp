528df0bd35c90a99cc64ce80f4529af48f07e748
nicoledanielle <nicolegilbert16@gmail.com>
2019-12-16 14:14:26 -0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP } from '@ionic-native/http/ngx'
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HTTP } from '@ionic-native/http/ngx'
+
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private http: HTTP) {}
 
+  public ngOnInit() {
+    this.http.get('https://community-open-weather-map.p.rapidapi.com/weather', {}, {}).then(_res => {
+      console.log(_res);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 }
